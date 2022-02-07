2872dcb033e7a0d89f92fde3325560c47a8570ac
Katey Almeida <77791996+klalmeida@users.noreply.github.com>
2022-02-06 21:11:38 -0500

    fixed welcome

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import express, {Request, Response} from 'express';
 const app = express();
 
 app.get('/', (req: Request, res: Response) =>
-    res.send('Welcome to my Node Project'));
+    res.send("Welcome to Katey's Node Project!!!"));
                  
 app.get('/hello', (req: Request, res: Response) =>
     res.send('Hello World!'));
