f27ada698c1692c319a0bd4dc591367cd8795f99
Katey Almeida <77791996+klalmeida@users.noreply.github.com>
2022-02-06 21:08:37 -0500

    add welcome code

--- server.ts
+++ server.ts
@@ -1,6 +1,9 @@
 import express, {Request, Response} from 'express';
 const app = express();
 
+app.get('/', (req: Request, res: Response) =>
+    res.send('Welcome to my Node Project'));
+                 
 app.get('/hello', (req: Request, res: Response) =>
     res.send('Hello World!'));
 
